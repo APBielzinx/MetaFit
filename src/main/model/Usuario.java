96c1a95982245c9d87227755c5aa5f67295699a8
Miriam <miriamgama456@gmail.com>
2024-11-18 22:30:32 -0300

    Usuario.java
    Neste código, criei a classe mãe "Usuário" para o projeto.

--- src/main/model/Usuario.java
+++ src/main/model/Usuario.java
@@ -1,5 +1,32 @@
 package main.model;
 
-public class Usuario {
+//Classe mãe Usuário
+
+//Atributos
+public class Usuario
+{
+private String nome;
+private String email;
+private String senha;
+private int tipo;
+
+//Construtor
+public Usuario(String nome, String email, String senha, int tipo){
+this.nome = nome;
+this.email = email;
+this.senha = senha;
+this.tipo = tipo;
+}
+
+//Métodos de acesso
+public String getNome(){ 
+return nome;
+}
+public String getEmail(){ 
+return email;
+}
+public String getSenha(){ 
+return nome;
+}
 
 }
